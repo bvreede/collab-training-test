2f07d25a2b012da7886dce6f2421ae83f027677f
Barbara Vreede <b.vreede@esciencecenter.nl>
2024-04-07 20:36:11 +0200

    add markdown and push with github actions

--- grocery_story.R
+++ grocery_story.R
@@ -1,25 +1,26 @@
-# people
+## collect the variables, and pick one at random
+
 people <- c("Jennifer", "Albert", "Tiago", "Oisin", "Miriam", "Linda", "Emmy", "Daniel")
 person <- sample(people, 1)
 
-# fruit
 fruits <- c("banana", "melon", "mango", "grape", "apple", "cucumber", "papaya")
 fruit <- sample(fruits, 1)
 
-# in the grocery store
 items <- c("toilet paper", "wine", "trash bags", "shampoo", "dog food")
 item <- sample(items, 1)
 
-# verbs
 actions <- c("smashed", "shredded", "opened", "crushed", "squeezed")
 verb <- sample(actions, 1)
 
-# modes of transport
 transportation <- c("ufo", "flying carpet", "ATV", "roller skates", "lawn mower")
 transportmode <- sample(transportation, 1)
 
-# judgement time
 judgings <- c("fabulous", "questionable", "top notch", "interesting", "wonderful")
 judgement <- sample(judgings, 1)
 
-madlibs::story_grocer(person, fruit, item, verb, transportmode, judgement)
+
+# Make the story
+story <- madlibs::story_grocer(person, fruit, item, verb, transportmode, judgement)
+
+# save story as markdown
+writeLines(story, con = "story.md")
